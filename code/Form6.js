d91dee54e2bec05fe70577b3948b65dc9f1fdae1
Phùng Xuân Đoan <90822988+XDAZK@users.noreply.github.com>
2023-11-20 18:55:55 +0700

    Add files via upload

---
+++ code/Form6.js
@@ -0,0 +1,9 @@
+document.getElementById("cauhoi1192dapan4").click();
+for (let i = 1194; i <= 1219; i++) {
+  try {
+    document.getElementById("cauhoi" + i + "dapan5").click();
+  } catch (error) {
+    continue;
+  }
+}
+document.querySelector(".btn-success").click();
